b266d547695475857160257243885e53ff6ba075
Akemi141 <davidgib@mail.ru>
2026-07-19 23:58:34 +0300

    Create lilygoS3TEmbedCC1101.h

---
+++ src/drivers/devices/lilygoS3TEmbedCC1101.h
@@ -0,0 +1,10 @@
+#ifndef _LILYGO_S3_T_EMBED_CC1101_H
+#define _LILYGO_S3_T_EMBED_CC1101_H
+
+// LilyGo T-Embed CC1101 - pins per https://github.com/Xinyuan-LilyGO/T-Embed-CC1101#4-pins
+#define PIN_BUTTON_1 0    // ENCODER_KEY - rotary encoder push button
+#define PIN_ENABLE5V 15   // BOARD_PWR_EN - enables power to the board peripherals (incl. display)
+
+#define T_DISPLAY
+
+#endif
