689996e29a32840abb26402a2f4cd58d36d1d2af
Akemi141 <davidgib@mail.ru>
2026-07-20 00:07:44 +0300

    Update User_Setup_Select.h

--- lib/TFT_eSPI/User_Setup_Select.h
+++ lib/TFT_eSPI/User_Setup_Select.h
@@ -149,6 +149,9 @@
 #ifdef LILYGO_S3_T_EMBED
 #include <User_Setups/Setup210_LilyGo_T_Embed_S3.h>         // For the LilyGo T-Embed S3 based ESP32S3 with ST7789 170 x 320 TFT
 #endif
+#ifdef LILYGO_S3_T_EMBED_CC1101
+#include <User_Setups/Setup216_LilyGo_T_Embed_CC1101.h>
+#endif
 #ifdef M5_CARDPUTER_ADV
 #include <User_Setups/Setup215_M5_Cardputer_Adv.h>          // M5Stack Cardputer Adv (ESP32-S3) with ST7789V2 240 x 135 TFT
 #endif
